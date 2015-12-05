5eede3bae6dbf31e82f7ae682192d852cc366476
Ramses Sotelo <rsotelo2@stedwards.edu>
2015-12-05 13:17:30 -0600

    Started Implementing Dropwizard Version

---
+++ src/main/java/com/pasotaku/main/AnimeFeudApplication.java
@@ -0,0 +1,37 @@
+package com.pasotaku.main;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+import com.example.helloworld.resources.HelloWorldResource;
+import com.example.helloworld.health.TemplateHealthCheck;
+
+public class AnimeFeudApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new AnimeFeudApplication().run(args);
+    }
+
+    public String getName(HelloWorldConfiguration configuration) {
+        return configuration.getApplicationName();
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+
+}
---
+++ src/main/java/com/pasotaku/main/HelloWorldConfiguration.java
@@ -0,0 +1,46 @@
+package com.pasotaku.main;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class HelloWorldConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+    
+    @NotEmpty
+    private String applicationName;
+    
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+    
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+    
+    @JsonProperty
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+    @JsonProperty
+    public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+}
